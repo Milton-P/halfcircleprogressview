3ffee12efbc47a9997256eef9c3ae7ad51185ecc
Milton-P <pmt206@126.com>
2017-06-16 11:05:12 +0800

    finish halfCircleProgressView ,add custom att and interface

--- app/src/main/java/com/test/milton/halfcircleprogressview/MainActivity.java
+++ app/src/main/java/com/test/milton/halfcircleprogressview/MainActivity.java
@@ -4,10 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    HalfCircleProgressView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = (HalfCircleProgressView) this.findViewById(R.id.halfCircle);
+        new  Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 1;
+                while (i <= 100){
+                    view.setProgress(i++);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
